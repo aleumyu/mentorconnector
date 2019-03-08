a3b0d699a0460b1b3b94976ed818bfe3e689a849
Aleum <41451720+aleumyu@users.noreply.github.com>
2019-03-08 13:03:45 +0100

    Basic set up (#1)
    * working on API
    
    * Favorites API works
    
    * changes as requested
    
    * pulled

--- api/routes/index.js
+++ api/routes/index.js
@@ -7,10 +7,14 @@ router.get('/', function(req, res, next) {
   res.send({ response: 'Express reponse' });
 });
 
-router.get('/api/v1/users/:id', (req, res, next) => {
-  db('SELECT * FROM user WHERE user.userId =' + `${req.params.id}`)
+
+
+router.get('/api/v1/users/:id/favorites', function(req, res, next) {
+  
+  db(`SELECT f.userId, photo, industry, jobType, location, firstName, lastName, f.selectedUserId FROM user u INNER JOIN favorites f ON u.userId=f.selectedUserId WHERE f.userId=${req.params.id};`)
     .then(results => {
       if (results.error) {
+        console.log('as');
         res.status(500).send(results.error);
       }
       console.log('results: ' + JSON.stringify(results.data));
@@ -18,88 +22,29 @@ router.get('/api/v1/users/:id', (req, res, next) => {
     })
 });
 
-// http://localhost:9000/api/v1/user?role=${roleParam}&industry=${industryParam}&meeting=${meetingParam}&interests=${interestsParam}&location=${locationParam}`
 
-router.get('/api/v1/users/', function (req, res, next) {
-  let industryParam = req.query.industry;
-  let locationParam = req.query.location;
-  let roleParam = req.query.role;
-  let meetingParam = req.query.meeting;
-  let interestsParam = req.query.interest;
-
-  if (!interestsParam && !roleParam && !meetingParam && !industryParam && !locationParam) {
-    db('SELECT * FROM user ORDER BY userId ASC;')
-      .then(results => {
-        if (results.error) {
-          res.status(500).send(results.error);
-        }
-        console.log('results: ' + JSON.stringify(results.data));
-        res.send(results.data);
-      })
-  } else if (interestsParam && roleParam && meetingParam && industryParam && locationParam) {
-    db(`SELECT u.userId, industry, location, role, meeting, firstName FROM user u INNER JOIN interests i ON u.userId=i.userId WHERE u.industry="${industryParam}" AND u.location="${locationParam}" AND u.role=${roleParam} AND u.meeting=${meetingParam} AND i.interestTag="${interestsParam}";`)
-      .then(results => {
-        if (results.error) {
-          res.status(500).send(results.error);
-        }
-        console.log('results: ' + JSON.stringify(results.data));
-        res.send(results.data);
-      })
-  } else { 
-    let baseQuery = "SELECT u.userId, industry, location, role, meeting, firstName FROM user u INNER JOIN interests i ON u.userId=i.userId WHERE ";
-    let fullQuery = baseQuery;
-    let containOtherQuery = false;
-
-    if (roleParam) {
-      roleParam = locParam.split(',').map( e => `u.role="${e}" OR u.role="2"`).join(' OR ');
-      fullQuery += `${roleParam}`;
-      containOtherQuery = true;
+router.post('/api/v1/users/:id/favorites', function(req, res, next) {
+  db(`INSERT INTO favorites (userId, selectedUserId) VALUES (${req.params.id}, ${req.body.selectedUserId});`)
+  .then(results => {
+    if (results.error) {
+      res.status(500).send(resutls.error);
     }
-    if (meetingParam) {
-      meetingParam = meetingParam.split(',').map( e => `u.meeting="${e}" OR u.meeting="2"`).join(' OR ');
-      if (containOtherQuery) {
-        fullQuery += `AND ${meetingParam}`;
-      } else {
-        fullQuery += `${meetingParam}`;
-        containOtherQuery = true;
-      }
-    }
-    if (interestsParam) {
-      interestsParam = interestsParam.split(',').map( e => `u.interests="${e}"`).join(' OR ');
-      if (containOtherQuery) {
-        fullQuery += `AND ${interestsParam}`;
-      } else {
-        fullQuery += `${interestsParam}`;
-        containOtherQuery = true;
-      }
-    }
-    if (industryParam) {
-      industryParam = industryParam.split(',').map( e => `u.industry="${e}"`).join(' OR ');
-      if (containOtherQuery) {
-        fullQuery += `AND ${industryParam}`;
-      } else {
-        fullQuery += `${industryParam}`;
-        containOtherQuery = true;
-      }
-    }
-    if (locationParam) {
-      locationParam = locationParam.split(',').map( e => `u.role="${e}" OR u.location="2"`).join(' OR ');
-      if (containOtherQuery) {
-        fullQuery += `AND ${locationParam}`;
-      } else {
-        fullQuery += `${locationParam}`;
-        containOtherQuery = true;
-      }
-    }
-    db(`${fullQuery};`)
-      .then(results => {
-        if (results.error) {
-          res.status(500).send(results.error);
-        }
-        console.log('results: ' + JSON.stringify(results.data));
-        res.send(results.data);
-      })
-  }
+    res.send(results.data);
+  })
 });
+
+router.delete('/api/v1/users/:id1/favorites/:id2', function(req, res, next) {
+  db(`DELETE FROM favorites WHERE userId=${req.params.id1} AND selectedUserId=${req.params.id2};`)
+  .then(results => {
+    if (results.error) {
+      res.status(500).send(results.error);
+    }
+    res.send(results.data);
+  })
+
+})
+
+
+
 
 module.exports = router;
