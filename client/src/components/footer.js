e4ef039d249eacb104d33d0ee9ff610e66939e4f
Aleum <41451720+aleumyu@users.noreply.github.com>
2019-03-12 12:19:42 +0100

    Men 15 (#2)
    * profile & favorites componentes
    
    * profile&favorite components work
    
    * db migration added
    
    * code modified as requested

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -13,8 +13,6 @@ class Footer extends Component {
         
         <div className="body">
 
-       
-
           <footer className="footer">
             <div className="container">
               <span className="text-muted">Place sticky footer content here.</span>
